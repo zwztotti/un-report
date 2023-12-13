3076b638ecee797da03e9eec8fc368ffb3081889
Wenzhen Zhu <zwztotti@163.com>
2023-12-13 15:27:48 -0500

    这段代码是分析GDP与人均寿命预期的关系

---
+++ code/gdp_population.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(readr)
+gapminder_1997 <- read_csv("gapminder_1997.csv")
+View(gapminder_1997)
+
+name <- "Ben"
+name
+age <- 26
+age
+
+name_character <- "Harry Potter"
+
+  
+test <- read_csv("gapminder_1997.csv")  # when to use double quotation mark?
+
+Sys.Date() #output the current date, used for knowing when I last ran
+
+getwd() #output current directory
+
+sum(5,6) #adds numbers
+
+round(3.1415, 3)
+read_csv(file = 'gapminder_1997.csv') # when to use single quotation mark?
+
+
+#Draw it step by step
+ggplot(data = gapminder_1997) + 
+  aes(x = gdpPercap) +
+  labs(x = "GDP Per Capita")+
+  aes(y = lifeExp)+
+  labs(y = "Life Expectancy")+
+  geom_point()+ #The form of drawing
+  labs(title = "Do people in wealthy countries live longer?")+
+  aes(color = continent)+
+  scale_color_brewer(palette = "Set1")+
+  aes(size = pop/1000000)+
+  labs(size = "Population (in millions)")+ #change the Size legend of the figure 
+  aes(shape = continent)
+
+#Draw it all at once
+ggplot(data = gapminder_1997)+
+  aes(x = gdpPercap, y = lifeExp, color = continent, size = pop/1000000, shape = continent)+
+  geom_point()+ 
+  scale_color_brewer(palette = "Set1")+
+  labs(x = "GDP Per Capita", y = "Life Expectancy",
+       title = "Do people in wealthy countries live longer?", 
+       size = "Population (in millions)")
+  
+gapminder_data <- read.csv("gapminder_data.csv")
+
+dim(gapminder_data)  #Preview the overall size of the data
+head(gapminder_data)  #Preview the first few rows of the data table
+
+ggplot(data = gapminder_data)+
+  aes(x = year, y = lifeExp, color = continent)+
+  geom_boxplot()
+  
+
